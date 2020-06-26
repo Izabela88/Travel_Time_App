1cd99d333b78c535f6575061a384f5dc75d2287e
izak <izabkru88@gmail.com>
2020-06-26 14:00:23 +0100

    Installed html webpack plugin,added a plugins list to the webpack config and instantiate the plugin.

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Express to run server and routes
-const express = require('express');
 var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
 // Start up an instance of app
 const app = express();
@@ -14,7 +15,17 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+  res.sendFile('dist/index.html')
+})
+
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
   console.log('Server 8081!')
+})
+
+app.get('/test', function (req, res) {
+  res.send(mockAPIResponse)
 })
