0a6f48836734ca7f9f42ca85302dd1ff4fb1991c
izak <izabkru88@gmail.com>
2020-07-01 13:21:56 +0100

    Rebuild and styled  My Trips page.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -38,9 +38,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.querySelector("#navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-250px";
+    document.querySelector("#navbar").style.top = "-250px";
   }
   prevScrollpos = currentScrollPos;
 }
